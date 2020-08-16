0a83791f5e761353c1575e5b50d0058947794e76
rasitha11 <rasithasen@gmail.com>
2020-08-17 01:24:12 +0530

    Messages interface raz final commit

--- app/src/main/java/com/drex/dashboard/Mesages_Interface_Raz.java
+++ app/src/main/java/com/drex/dashboard/Mesages_Interface_Raz.java
@@ -3,6 +3,7 @@ package com.drex.dashboard;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Mesages_Interface_Raz extends AppCompatActivity {
 
@@ -10,5 +11,36 @@ public class Mesages_Interface_Raz extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mesages__interface__raz);
+
+        TextView Mesages_Interface_Header = findViewById(R.id.Mesages_Interface_HeaderTV);
+        Mesages_Interface_Header.setText(R.string.Mesages_Interface_Header);
+
+        TextView Sender_name1 = findViewById(R.id.Sender_Name_1TV);
+        Sender_name1.setText(R.string.sender_name);
+        TextView Sender_msg1 = findViewById(R.id.MSG_Preview_1TV);
+        Sender_msg1.setText(R.string.sender_msg);
+
+        TextView Sender_name2 = findViewById(R.id.Sender_Name_2TV);
+        Sender_name2.setText(R.string.sender_name);
+        TextView Sender_msg2 = findViewById(R.id.MSG_Preview_2TV);
+        Sender_msg2.setText(R.string.sender_msg);
+
+        TextView Sender_name3 = findViewById(R.id.Sender_Name_3TV);
+        Sender_name3.setText(R.string.sender_name);
+        TextView Sender_msg3 = findViewById(R.id.MSG_Preview_3TV);
+        Sender_msg3.setText(R.string.sender_msg);
+
+        TextView Sender_name4 = findViewById(R.id.Sender_Name_4TV);
+        Sender_name4.setText(R.string.sender_name);
+        TextView Sender_msg4 = findViewById(R.id.MSG_Preview_4TV);
+        Sender_msg4.setText(R.string.sender_msg);
+
+        TextView Sender_name5 = findViewById(R.id.Sender_Name_5TV);
+        Sender_name5.setText(R.string.sender_name);
+        TextView Sender_msg5 = findViewById(R.id.MSG_Preview_5TV);
+        Sender_msg5.setText(R.string.sender_msg);
+
+
+
     }
 }
