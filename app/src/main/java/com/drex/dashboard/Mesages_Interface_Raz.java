e0e5dae481e268cd6afda6ec01a6ee03cf97f38e
rasitha11 <rasithasen@gmail.com>
2020-08-17 00:41:38 +0530

    Merge branch 'master' of C:\Users\user\AndroidStudioProjects\MAD_app with conflicts.

---
+++ app/src/main/java/com/drex/dashboard/Mesages_Interface_Raz.java
@@ -0,0 +1,14 @@
+package com.drex.dashboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Mesages_Interface_Raz extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mesages__interface__raz);
+    }
+}
