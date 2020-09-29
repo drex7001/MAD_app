8ca16a07e05cc5f93ad503de3a5fc82a3775ef91
UltravioletDarling <iambimsara@gmail.com>
2020-09-29 16:41:53 +0530

    Major Changes

---
+++ app/src/main/java/com/drex/dashboard/Quiz.java
@@ -0,0 +1,287 @@
+package com.drex.dashboard;
+
+public class Quiz {
+
+    private String q1title,q1answer1,q1answer2,q1answer3,q1answer4,q1answer5,
+            q2title,q2answer1,q2answer2,q2answer3,q2answer4,q2answer5,
+            q3title,q3answer1,q3answer2,q3answer3,q3answer4,q3answer5,
+            q4title,q4answer1,q4answer2,q4answer3,q4answer4,q4answer5,
+            q5title,q5answer1,q5answer2,q5answer3,q5answer4,q5answer5;
+
+    public Quiz(String q1title, String q1answer1, String q1answer2, String q1answer3, String q1answer4, String q1answer5, String q2title, String q2answer1, String q2answer2, String q2answer3, String q2answer4, String q2answer5, String q3title, String q3answer1, String q3answer2, String q3answer3, String q3answer4, String q3answer5, String q4title, String q4answer1, String q4answer2, String q4answer3, String q4answer4, String q4answer5, String q5title, String q5answer1, String q5answer2, String q5answer3, String q5answer4, String q5answer5) {
+        this.q1title = q1title;
+        this.q1answer1 = q1answer1;
+        this.q1answer2 = q1answer2;
+        this.q1answer3 = q1answer3;
+        this.q1answer4 = q1answer4;
+        this.q1answer5 = q1answer5;
+        this.q2title = q2title;
+        this.q2answer1 = q2answer1;
+        this.q2answer2 = q2answer2;
+        this.q2answer3 = q2answer3;
+        this.q2answer4 = q2answer4;
+        this.q2answer5 = q2answer5;
+        this.q3title = q3title;
+        this.q3answer1 = q3answer1;
+        this.q3answer2 = q3answer2;
+        this.q3answer3 = q3answer3;
+        this.q3answer4 = q3answer4;
+        this.q3answer5 = q3answer5;
+        this.q4title = q4title;
+        this.q4answer1 = q4answer1;
+        this.q4answer2 = q4answer2;
+        this.q4answer3 = q4answer3;
+        this.q4answer4 = q4answer4;
+        this.q4answer5 = q4answer5;
+        this.q5title = q5title;
+        this.q5answer1 = q5answer1;
+        this.q5answer2 = q5answer2;
+        this.q5answer3 = q5answer3;
+        this.q5answer4 = q5answer4;
+        this.q5answer5 = q5answer5;
+    }
+
+    public Quiz() {
+
+    }
+
+    public String getQ1title() {
+        return q1title;
+    }
+
+    public void setQ1title(String q1title) {
+        this.q1title = q1title;
+    }
+
+    public String getQ1answer1() {
+        return q1answer1;
+    }
+
+    public void setQ1answer1(String q1answer1) {
+        this.q1answer1 = q1answer1;
+    }
+
+    public String getQ1answer2() {
+        return q1answer2;
+    }
+
+    public void setQ1answer2(String q1answer2) {
+        this.q1answer2 = q1answer2;
+    }
+
+    public String getQ1answer3() {
+        return q1answer3;
+    }
+
+    public void setQ1answer3(String q1answer3) {
+        this.q1answer3 = q1answer3;
+    }
+
+    public String getQ1answer4() {
+        return q1answer4;
+    }
+
+    public void setQ1answer4(String q1answer4) {
+        this.q1answer4 = q1answer4;
+    }
+
+    public String getQ1answer5() {
+        return q1answer5;
+    }
+
+    public void setQ1answer5(String q1answer5) {
+        this.q1answer5 = q1answer5;
+    }
+
+    public String getQ2title() {
+        return q2title;
+    }
+
+    public void setQ2title(String q2title) {
+        this.q2title = q2title;
+    }
+
+    public String getQ2answer1() {
+        return q2answer1;
+    }
+
+    public void setQ2answer1(String q2answer1) {
+        this.q2answer1 = q2answer1;
+    }
+
+    public String getQ2answer2() {
+        return q2answer2;
+    }
+
+    public void setQ2answer2(String q2answer2) {
+        this.q2answer2 = q2answer2;
+    }
+
+    public String getQ2answer3() {
+        return q2answer3;
+    }
+
+    public void setQ2answer3(String q2answer3) {
+        this.q2answer3 = q2answer3;
+    }
+
+    public String getQ2answer4() {
+        return q2answer4;
+    }
+
+    public void setQ2answer4(String q2answer4) {
+        this.q2answer4 = q2answer4;
+    }
+
+    public String getQ2answer5() {
+        return q2answer5;
+    }
+
+    public void setQ2answer5(String q2answer5) {
+        this.q2answer5 = q2answer5;
+    }
+
+    public String getQ3title() {
+        return q3title;
+    }
+
+    public void setQ3title(String q3title) {
+        this.q3title = q3title;
+    }
+
+    public String getQ3answer1() {
+        return q3answer1;
+    }
+
+    public void setQ3answer1(String q3answer1) {
+        this.q3answer1 = q3answer1;
+    }
+
+    public String getQ3answer2() {
+        return q3answer2;
+    }
+
+    public void setQ3answer2(String q3answer2) {
+        this.q3answer2 = q3answer2;
+    }
+
+    public String getQ3answer3() {
+        return q3answer3;
+    }
+
+    public void setQ3answer3(String q3answer3) {
+        this.q3answer3 = q3answer3;
+    }
+
+    public String getQ3answer4() {
+        return q3answer4;
+    }
+
+    public void setQ3answer4(String q3answer4) {
+        this.q3answer4 = q3answer4;
+    }
+
+    public String getQ3answer5() {
+        return q3answer5;
+    }
+
+    public void setQ3answer5(String q3answer5) {
+        this.q3answer5 = q3answer5;
+    }
+
+    public String getQ4title() {
+        return q4title;
+    }
+
+    public void setQ4title(String q4title) {
+        this.q4title = q4title;
+    }
+
+    public String getQ4answer1() {
+        return q4answer1;
+    }
+
+    public void setQ4answer1(String q4answer1) {
+        this.q4answer1 = q4answer1;
+    }
+
+    public String getQ4answer2() {
+        return q4answer2;
+    }
+
+    public void setQ4answer2(String q4answer2) {
+        this.q4answer2 = q4answer2;
+    }
+
+    public String getQ4answer3() {
+        return q4answer3;
+    }
+
+    public void setQ4answer3(String q4answer3) {
+        this.q4answer3 = q4answer3;
+    }
+
+    public String getQ4answer4() {
+        return q4answer4;
+    }
+
+    public void setQ4answer4(String q4answer4) {
+        this.q4answer4 = q4answer4;
+    }
+
+    public String getQ4answer5() {
+        return q4answer5;
+    }
+
+    public void setQ4answer5(String q4answer5) {
+        this.q4answer5 = q4answer5;
+    }
+
+    public String getQ5title() {
+        return q5title;
+    }
+
+    public void setQ5title(String q5title) {
+        this.q5title = q5title;
+    }
+
+    public String getQ5answer1() {
+        return q5answer1;
+    }
+
+    public void setQ5answer1(String q5answer1) {
+        this.q5answer1 = q5answer1;
+    }
+
+    public String getQ5answer2() {
+        return q5answer2;
+    }
+
+    public void setQ5answer2(String q5answer2) {
+        this.q5answer2 = q5answer2;
+    }
+
+    public String getQ5answer3() {
+        return q5answer3;
+    }
+
+    public void setQ5answer3(String q5answer3) {
+        this.q5answer3 = q5answer3;
+    }
+
+    public String getQ5answer4() {
+        return q5answer4;
+    }
+
+    public void setQ5answer4(String q5answer4) {
+        this.q5answer4 = q5answer4;
+    }
+
+    public String getQ5answer5() {
+        return q5answer5;
+    }
+
+    public void setQ5answer5(String q5answer5) {
+        this.q5answer5 = q5answer5;
+    }
+}
